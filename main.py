b32e9282304cc7a4356b4eeed052da6b946150dd
LEEjaeHyun <swaa23@gmail.com>
2020-05-11 15:25:59 +0900

    webcam to flask server

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, Response
+import cv2
+import socket
+import io
+
+# Flask 서버 설정
+app = Flask(__name__)
+# cv2 사용 0번째 카메라로 video캡쳐 시작
+vc = cv2.VideoCapture(0)
+
+
+@app.route('/')
+def index():
+    # 비디오 캡처 시작
+    """Video streaming ."""
+    return render_template('index.html')
+
+
+def gen():
+    # 비디오 캡처기능
+    """Video streaming generator function."""
+    while True:
+        rval, frame = vc.read()
+        cv2.imwrite('pic.jpg', frame)
+        
+        #제네레이터를 사용하여 객채를읽어옴 
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + open('pic.jpg', 'rb').read() + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """비디오스트리밍 경로 .{{ url_for('video_feed') }} 지정."""
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
