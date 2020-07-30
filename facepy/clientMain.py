8008b3eacbe86cf87c2b7ce050f2b17e249b7a60
LEEjaeHyun <swaa23@gmail.com>
2020-07-30 16:24:10 +0900

    임시 로그인

--- facepy/clientMain.py
+++ facepy/clientMain.py
@@ -1,12 +1,29 @@
 import cv2
 import face_recognition
+import hashlib
+
 import numpy as np
 import pickle
 import socket
 import struct
 import sys
+from table import CRUDdatabase
 
 
+def login(id, pw):
+    # GUI 구현
+    db = CRUDdatabase()
+    pw = hashlib.sha256(pw.encode())
+    pw = pw.hexdigest()
+    print(id, pw)
+    if db.login(id, pw) == True:
+        print('Login Success')
+        # TODO 로그인후 켐 로직 실행
+    else:
+        print('Login False')
+
+
+login('bcbc', 'bcbc')
 """사용자 실시간 얼굴 서버 전송로직"""
 cap = cv2.VideoCapture(0)
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,7 +34,7 @@ while True:
     data = pickle.dumps(frame)
 
     # Send message length first
-    cv2.imshow("VIdeo", frame)
+    #cv2.imshow("VIdeo", frame)
     face_landmarks_list = face_recognition.face_landmarks(frame)
     if face_landmarks_list:
         face_locations = face_recognition.face_locations(frame)
--- facepy/table.py
+++ facepy/table.py
@@ -4,6 +4,7 @@ from database import Database
 class CRUDdatabase(Database):
 
     """DB 이미지 가져오기 """
+
     def get_Image(self, no):
 
         sql = "SELECT image_file_str "
@@ -20,6 +21,7 @@ class CRUDdatabase(Database):
         return result
 
     """DB 이미지 저장 """
+
     def insert_Image(self, img_str):
 
         sql = "INSERT INTO student(image_file_str)"
@@ -45,4 +47,19 @@ class CRUDdatabase(Database):
         except Exception as e:
             result = {"error": "{}".format(e)}
 
+        return result
+
+    def login(self, id, passwd):
+        sql = "SELECT student_ID,pw "
+        sql += "FROM info "
+        sql += "WHERE student_ID='{}';".format(id)
+        result = False
+        try:
+            onerow = self.executeOne(sql)
+            if passwd == onerow.get('pw'):
+                result = True
+
+        except Exception as e:
+            return {"error": "{}".format(e)}
+        print(result)
         return result
