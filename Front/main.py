3dc2f9a0d5f60cee9eab354d069ae28c26877d65
LEEjaeHyun <swaa23@gmail.com>
2020-08-09 19:58:37 +0900

    쿠키 값으로 유저 사진 값 등록 가능

--- Front/main.py
+++ Front/main.py
@@ -14,7 +14,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'BCODE_Flask'
 socketio = SocketIO(app)
 vc = cv2.VideoCapture(0)
-tmepUserID = ''
 
 
 @app.route('/signup/info', methods=['GET', 'POST'])
@@ -30,7 +29,7 @@ def sigUP():
         db = Signdatabase()
         db.insert_studentInfo(studendt_no, studendt_pass,
                               studendt_name, student_ID)
-        return redirect('Registration')
+        return redirect('../signin')
     return render_template('signup.html')
 
 
@@ -38,16 +37,19 @@ def sigUP():
 @app.route('/signup/Registration', methods=['GET', 'POST'])
 def registration():
     """Video streaming ."""
-    if request.method == 'POST':
-        print('애초에여기타긴타냐?')
-        vc.release()
-        cv2.destroyAllWindows()
-    return render_template('index.html')
+    if login_status() == False:
+        if request.method == 'POST':
+            print('애초에여기타긴타냐?')
+            vc.release()
+            cv2.destroyAllWindows()
+        return render_template('index.html')
+    elif login_status() == True:
+        return '로그인 인증이 안됬습니다. '
 
 
 @app.route("/signin", methods=['GET', 'POST'])
 def signIn():
-    if cookie_status() == b'':
+    if login_status() == True:
         if request.method == 'POST':
             db = Signdatabase()
             pw = hashlib.sha256(request.form['password'].encode())
@@ -62,27 +64,26 @@ def signIn():
                 custom_resp = Response("COkkie 설정")
                 custom_resp.set_cookie("USERID", userid)
 
-                global tmepUserID
-                tmepUserID = userid
                 return custom_resp
             elif result == False:
                 return '아이디 또는 비밀번호가 다릅니다. '
-    elif cookie_status != b'':
+    elif login_status() == False:
         return '이미 로그인 되어져 있습니다! '
     return render_template('signin.html')
 
 
-def gen():
+def gen(cookie):
     # 비디오 캡처기능
     """Video streaming generator function."""
-    os.makedirs('./UserImage/{}'.format(tmepUserID), exist_ok=True)
+    os.makedirs('./UserImage/{}'.format(cookie), exist_ok=True)
     vc = cv2.VideoCapture(0)
     while (vc.isOpened()):
+        # 무한루프 깨는 로직 필요 ... 흠 .. 너무 세드하네 ;; 
         rval, frame = vc.read()
         cv2.imwrite(
-            './UserImage/{0}/{1}.jpg'.format(tmepUserID, tmepUserID+'firstImage'), frame)
+            './UserImage/{0}/{1}.jpg'.format(cookie, cookie+'firstImage'), frame)
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + open('./UserImage/{0}/{1}.jpg'.format(tmepUserID, tmepUserID+'firstImage'), 'rb').read() + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + open('./UserImage/{0}/{1}.jpg'.format(cookie, cookie+'firstImage'), 'rb').read() + b'\r\n')
         if cv2.waitKey(1) & 0xFF == 27:
             break
 
@@ -90,7 +91,8 @@ def gen():
 @app.route('/video_feed')
 def video_feed():
     """비디오스트리밍 경로 .{{ url_for('video_feed') }} 지정."""
-    return Response(gen(),
+    mkStr = cookie_status()
+    return Response(gen(mkStr),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
@@ -115,8 +117,24 @@ def clientAction():
 def cookie_status():
     tempstr = request.cookies.get('USERID', '빈문자열')
     tempstr = tempstr.encode("UTF-8")
-    return base64.b64decode(tempstr)
+
+    return (base64.b64decode(tempstr)).decode('UTF-8')
+
+# 로그인 상태 여부 확인
+def login_status():
+   
+    # 로그인이 안되어있을 때
+    if cookie_status() == '':
+        return True
+    # 로그인이 되어 있을 때 
+    elif cookie_status() != '':
+        return False
 
 
 if __name__ == '__main__':
     socketio.run(app, port=5000, debug=True)
+
+
+# 필요한거 네브바
+# 함수 처리후 다음 페이지로 자연스럽게 이동
+# 흠 ...
